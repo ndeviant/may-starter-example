7621859cc4a794b610994f4ebec511b2c0028421
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-05-17 15:54:48 +0300

    Added slight description to the gulp options

--- gulp.options.js
+++ gulp.options.js
@@ -1,3 +1,15 @@
+/**
+ * Here you can manage your build process, without going through
+ * tons of files, and modifing them.
+ */
+
+/**
+ * Root
+ * @typedef {Object} root - Note that tasks path's reffering to the root property by default.
+ * @property {string} src - The path to source files of you project
+ * @property {string} dist - The path where to place compiled files
+ */
+
 const options = {
 	root: {
 		src: "",
