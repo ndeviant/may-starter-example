0293559477f84c0477fffc8fa2f2302144fb482d
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-05-17 17:10:36 +0300

    slightly midided gulp options

--- gulp.options.js
+++ gulp.options.js
@@ -3,48 +3,58 @@
  * tons of files, and modifing them.
  */
 
+/**
+ *
+ * Root
+ *
+ * @typedef {Object} root - Note that tasks path's reffering to the root property by default.
+ * @property {string} src - The path to source files of you project.
+ * @property {string} dist - The path where to place compiled files.
+ */
+
+const root = {};
+
+/**
+ *
+ * BrowserSync
+ *
+ * @typedef {Object} browserSync - Docs: browsersync.io/docs/options
+ */
+
+const browserSync = {};
+
+/**
+ *
+ * Tasks
+ *
+ * @typedef {Object} tasks - Each prop is reffering to existing task.
+ * @property {Object} [task] - Any task.
+ *	 @property {bool} run - Disable any task, by setting this to false.
+ *	 @property {string} src - Where to get specific files for the task,
+ * 	 by default starts from "root.src" prop.
+ *	 @property {string} dist - Where to place compiled files,
+ *	 by default starts from "root.dist" prop.
+ *	 @property {string} watch - Which files should gulp watch,
+ *	 by default starts from "root.src" prop.
+ */
+
+const tasks = {
+	cleanFiles: {},
+	htaccess: {},
+	views: {},
+	styles: {},
+	scripts: {},
+	images: {},
+	webp: {},
+	favs: {},
+	svg: {},
+	fonts: {},
+};
+
 const options = {
-	/**
-	 * Root
-	 * @typedef {Object} root - Note that tasks path's reffering to the root property by default.
-	 * @property {string} src - The path to source files of you project.
-	 * @property {string} dist - The path where to place compiled files.
-	 */
-
-	root: {},
-
-	/**
-	 * BrowserSync
-	 * @typedef {Object} browserSync - Docs: browsersync.io/docs/options
-	 */
-
-	browserSync: {},
-
-	/**
-	 * Tasks
-	 * @typedef {Object} tasks - Each prop is reffering to existing task.
-	 * @property {Object} [task] - Any task.
-	 *	 @property {bool} run - Disable any task, by setting this to false.
-	 *	 @property {string} src - Where to get specific files for the task,
-	 * 	 by default starts from "root.src" prop.
-	 *	 @property {string} dist - Where to place compiled files,
-	 *	 by default starts from "root.dist" prop.
-	 *	 @property {string} watch - Which files should gulp watch,
-	 *	 by default starts from "root.src" prop.
-	 */
-
-	tasks: {
-		cleanFiles: {},
-		htaccess: {},
-		views: {},
-		styles: {},
-		scripts: {},
-		images: {},
-		webp: {},
-		favs: {},
-		svg: {},
-		fonts: {},
-	},
+	root,
+	browserSync,
+	tasks,
 };
 
 export { options };
