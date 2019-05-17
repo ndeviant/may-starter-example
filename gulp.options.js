73ce5b95cf3ce9262a1fee538f232b914f94ec6c
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-05-17 16:18:08 +0300

    Added more comments to the gulp.options

--- gulp.options.js
+++ gulp.options.js
@@ -3,50 +3,47 @@
  * tons of files, and modifing them.
  */
 
-/**
- * Root
- * @typedef {Object} root - Note that tasks path's reffering to the root property by default.
- * @property {string} src - The path to source files of you project
- * @property {string} dist - The path where to place compiled files
- */
-
 const options = {
-	root: {
-		src: "",
-		dist: "",
-	},
+	/**
+	 * Root
+	 * @typedef {Object} root - Note that tasks path's reffering to the root property by default.
+	 * @property {string} src - The path to source files of you project.
+	 * @property {string} dist - The path where to place compiled files.
+	 */
+
+	root: {},
+
+	/**
+	 * BrowserSync
+	 * @typedef {Object} browserSync - Docs: browsersync.io/docs/options
+	 */
+
 	browserSync: {},
+
+	/**
+	 * Tasks
+	 * @typedef {Object} tasks - Each prop is reffering to existing task.
+	 * @property {Object} [task] - Any task.
+	 *	 @property {bool} run - Disable any task, by setting this to false.
+	 *	 @property {string} src - Where to get specific files for the task,
+	 * 	 by default starts from "root.src" prop.
+	 *	 @property {string} dist - Where to place compiled files,
+	 *	 by default starts from "root.dist" prop.
+	 *	 @property {string} watch - Which files should gulp watch,
+	 *	 by default starts from "root.src" prop.
+	 */
+
 	tasks: {
-		cleanFiles: {
-			run: true,
-		},
-		htaccess: {
-			run: true,
-		},
-		views: {
-			run: true,
-		},
-		styles: {
-			run: true,
-		},
-		scripts: {
-			run: true,
-		},
-		images: {
-			run: true,
-		},
-		webp: {
-			run: true,
-		},
-		favs: {
-			run: true,
-		},
-		svg: {
-			run: true,
-		},
-		fonts: {
-			run: true,
-		},
+		cleanFiles: {},
+		htaccess: {},
+		views: {},
+		styles: {},
+		scripts: {},
+		images: {},
+		webp: {},
+		favs: {},
+		svg: {},
+		fonts: {},
 	},
 };
 
