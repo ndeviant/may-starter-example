35da9b3e3cc14d827358126b091d8c1180213320
Nikolai Kolosovskyi <kpoccep@yandex.ru>
2019-05-19 13:22:19 +0300

    Changes in gulp config

--- tasks/helpers/gulp.config.js
+++ tasks/helpers/gulp.config.js
@@ -1,11 +1,20 @@
 import { options as userOptions } from "../../gulp.options";
 
+/**
+ * Root paths:
+ * Default root path's
+ */
 const root = {
 	src: userOptions.root.src || "./src",
 	dist: userOptions.root.dist || "./dist",
 };
 
 root.assets = userOptions.root.assets || `${root.dist}/assets`;
+
+/**
+ * Bsync config:
+ * Serve dist dir, disables nitifies, disables online, unless tunnel is on
+ */
 
 const bsyncConfig = {
 	server: root.dist,
@@ -15,82 +24,157 @@ const bsyncConfig = {
 	...userOptions.browserSync,
 };
 
-const tasks = {
-	cleanFiles: {
-		run: true,
-		...userOptions.tasks.cleanFiles,
-	},
-	views: {
-		src: [`${root.src}/views/pages/*.htm`],
-		dist: root.dist,
-		watch: [`${root.src}/components/**/*.htm`, `${root.src}/views/**/*.htm`],
-		run: true,
-		...userOptions.tasks.views,
-	},
-	styles: {
-		src: `${root.src}/sass/main.scss`,
-		dist: `${root.assets}/css/`,
-		watch: [`${root.src}/components/**/*.scss`, `${root.src}/sass/**/*.scss`],
-		run: true,
-		...userOptions.tasks.styles,
-	},
-	scripts: {
-		src: `${root.src}/js/index.js`,
-		dist: `${root.assets}/js/`,
-		watch: [`${root.src}/components/**/*.js`, `${root.src}/js/**/*.js`],
-		run: true,
-		...userOptions.tasks.scripts,
-	},
-	images: {
-		src: [
-			`${root.src}/images/**/*.{jpg,jpeg,png,gif,svg}`,
-			`!${root.src}/images/svg/*.svg`,
-			`!${root.src}/images/favicon.{jpg,jpeg,png,gif,svg}`,
-		],
-		dist: `${root.dist}/assets/images/`,
-		watch: [
-			`${root.src}/images/**/*.{jpg,jpeg,png,gif,svg}`,
-			`!${root.src}/images/svg/*.svg`,
-			`!${root.src}/images/favicon.{jpg,jpeg,png,gif,svg}`,
-		],
-		run: true,
-		...userOptions.tasks.images,
-	},
-	webp: {
-		src: `${root.src}/images/**/*_webp.{jpg,jpeg,png}`,
-		dist: `${root.assets}/images/`,
-		watch: `${root.src}/images/**/*_webp.{jpg,jpeg,png}`,
-		run: true,
-		...userOptions.tasks.webp,
-	},
-	fonts: {
-		src: `${root.src}/fonts/**/*.{ttf,otf,woff,woff2}`,
-		dist: `${root.assets}/fonts/`,
-		watch: `${root.src}/fonts/**/*.{ttf,otf,woff,woff2}`,
-		run: true,
-		...userOptions.tasks.fonts,
-	},
-	favs: {
-		src: `${root.src}/images/favicon.{jpg,jpeg,png,gif,svg}`,
-		dist: `${root.assets}/images/favicons/`,
-		watch: `${root.src}/images/favicon.{jpg,jpeg,png,gif,svg}`,
-		run: true,
-		...userOptions.tasks.favs,
-	},
-	svg: {
-		src: `${root.src}/images/svg/*.svg`,
-		dist: `${root.assets}/images/`,
-		watch: `${root.src}/images/svg/*.svg`,
-		run: true,
-		...userOptions.tasks.svg,
-	},
-	htaccess: {
-		src: `${root.src}/.htaccess`,
-		dist: root.dist,
-		run: true,
-		...userOptions.tasks.fonts,
-	},
+/**
+ * Tasks config:
+ * Default configs for tasks. Has default `run: true`, on each.
+ * Has `src, dist, watch` fields. Watch is often reffering to src.
+ */
+
+/**
+ *  Clean files
+ */
+
+const cleanFiles = {
+	run: true,
+	...userOptions.tasks.cleanFiles,
 };
+
+/**
+ * Views
+ */
+
+const views = {
+	src: [`${root.src}/views/pages/*.htm`],
+	dist: root.dist,
+	watch: [`${root.src}/components/**/*.htm`, `${root.src}/views/**/*.htm`],
+	run: true,
+	...userOptions.tasks.views,
+};
+
+/**
+ * Styles
+ */
+
+const styles = {
+	src: `${root.src}/sass/main.scss`,
+	dist: `${root.assets}/css/`,
+	watch: [`${root.src}/components/**/*.scss`, `${root.src}/sass/**/*.scss`],
+	run: true,
+	...userOptions.tasks.styles,
+};
+
+/**
+ * Scripts
+ */
+
+const scripts = {
+	src: `${root.src}/js/index.js`,
+	dist: `${root.assets}/js/`,
+	watch: [`${root.src}/components/**/*.js`, `${root.src}/js/**/*.js`],
+	run: true,
+	...userOptions.tasks.scripts,
+};
+
+/**
+ * Images
+ */
+
+const images = {
+	src: [
+		`${root.src}/images/**/*.{jpg,jpeg,png,gif,svg}`,
+		`!${root.src}/images/svg/*.svg`,
+		`!${root.src}/images/favicon.{jpg,jpeg,png,gif,svg}`,
+	],
+	dist: `${root.dist}/assets/images/`,
+	run: true,
+	...userOptions.tasks.images,
+};
+
+images.watch = userOptions.tasks.images.watch || images.src;
+
+/**
+ * WebP
+ */
+
+const webp = {
+	src: `${root.src}/images/**/*_webp.{jpg,jpeg,png}`,
+	dist: `${root.assets}/images/`,
+	run: true,
+	...userOptions.tasks.webp,
+};
+
+webp.watch = userOptions.tasks.webp.watch || webp.src;
+
+/**
+ * Fonts
+ */
+
+const fonts = {
+	src: `${root.src}/fonts/**/*.{ttf,otf,woff,woff2}`,
+	dist: `${root.assets}/fonts/`,
+	run: true,
+	...userOptions.tasks.fonts,
+};
+
+fonts.watch = userOptions.tasks.fonts.watch || fonts.src;
+
+/**
+ * Favs
+ */
+
+const favs = {
+	src: `${root.src}/images/favicon.{jpg,jpeg,png,gif,svg}`,
+	dist: `${root.assets}/images/favicons/`,
+	run: true,
+	...userOptions.tasks.favs,
+};
+
+favs.watch = userOptions.tasks.favs.watch || favs.src;
+
+/**
+ * Svg
+ */
+
+const svg = {
+	src: `${root.src}/images/svg/*.svg`,
+	dist: `${root.assets}/images/`,
+	run: true,
+	...userOptions.tasks.svg,
+};
+
+svg.watch = userOptions.tasks.svg.watch || svg.src;
+
+/**
+ * Htaccess
+ */
+
+const htaccess = {
+	src: `${root.src}/.htaccess`,
+	dist: root.dist,
+	run: true,
+	...userOptions.tasks.fonts,
+};
+
+/**
+ * All tasks
+ */
+
+const tasks = {
+	cleanFiles,
+	views,
+	styles,
+	scripts,
+	images,
+	webp,
+	fonts,
+	favs,
+	svg,
+	htaccess,
+};
+
+/**
+ * Result config
+ */
 
 const config = {
 	root,
