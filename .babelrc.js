37ab7d80880de8ba593fdb72544b4ecaf2cca5b1
Nikolai Kolosovskyi <mykola@oneplanetops.com>
2019-05-21 14:25:32 +0300

    added couple of plugins to babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,9 @@
 module.exports = {
 	presets: [
 		require.resolve("@babel/preset-env")
+	],
+	plugins: [
+		"@babel/plugin-syntax-dynamic-import",
+		"@babel/plugin-proposal-class-properties"
 	]
 };
